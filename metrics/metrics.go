5a83b2b5b492eb3584352b1f99326e2105c5b063
MoeMahhouk <mohammed-mahhouk@hotmail.com>
2024-10-07 15:04:08 +0000

    Run make lint, make test, go mod tidy

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -2,9 +2,7 @@ package metrics
 
 import "github.com/VictoriaMetrics/metrics"
 
-var (
-	totalRequestReceived = metrics.NewCounter("requests_total")
-)
+var totalRequestReceived = metrics.NewCounter("requests_total")
 
 func IncRequestsReceived() {
 	totalRequestReceived.Inc()
