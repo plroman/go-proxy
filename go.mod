3f1575dc7346420e7727533be8c12a0bb57a786a
Vitaly Drogan <vitaliy.drogan@gmail.com>
2024-10-08 17:11:36 +0200

    signature

--- go.mod
+++ go.mod
@@ -5,24 +5,32 @@ go 1.22
 require (
 	github.com/VictoriaMetrics/metrics v1.35.1
 	github.com/ethereum/go-ethereum v1.14.10
+	github.com/flashbots/go-utils v0.8.0
 	github.com/google/uuid v1.6.0
 	github.com/urfave/cli/v2 v2.27.2
 )
 
 require (
+	github.com/bits-and-blooms/bitset v1.13.0 // indirect
 	github.com/btcsuite/btcd/btcec/v2 v2.3.4 // indirect
+	github.com/consensys/bavard v0.1.13 // indirect
+	github.com/consensys/gnark-crypto v0.12.1 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.4 // indirect
+	github.com/crate-crypto/go-ipa v0.0.0-20240223125850-b1e8a79f509c // indirect
+	github.com/crate-crypto/go-kzg-4844 v1.0.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.2.0 // indirect
-	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/ethereum/c-kzg-4844 v1.0.0 // indirect
+	github.com/ethereum/go-verkle v0.1.1-0.20240829091221-dffa7562dbe9 // indirect
 	github.com/holiman/uint256 v1.3.1 // indirect
+	github.com/mmcloughlin/addchain v0.4.0 // indirect
+	github.com/rogpeppe/go-internal v1.12.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/supranational/blst v0.3.13 // indirect
 	github.com/valyala/fastrand v1.1.0 // indirect
 	github.com/valyala/histogram v1.2.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20240312152122-5f08fbb34913 // indirect
 	golang.org/x/crypto v0.22.0 // indirect
-	golang.org/x/mod v0.17.0 // indirect
 	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.22.0 // indirect
-	golang.org/x/tools v0.20.0 // indirect
-	mvdan.cc/gofumpt v0.7.0 // indirect
+	rsc.io/tmplfunc v0.0.3 // indirect
 )
--- proxy/request_processor.go
+++ proxy/request_processor.go
@@ -4,23 +4,49 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log/slog"
 	"net/http"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/flashbots/go-utils/signature"
 )
 
 const maxRequestBodySizeBytes = 10 * 1024 * 1024 // 10 MB, @configurable
 
+// Errors returned by the orderflow proxy
+// all errors are HTTP 200 jsonrpc responses except some that return proper http error code and a string
 var (
-	errMethodNotAllowed = "Method not allowed"
+	errMethodNotAllowed = "Only POST method is allowded"
 	errWrongContentType = "Content-Type must be application/json"
-	errBodySize         = fmt.Sprintf("Request body too large, max body size %d", maxRequestBodySizeBytes)
-	errInvalidJSON      = "Invalid JSON request"
 	errMarshalResponse  = "Failed to marshal response"
 
+	errBodySize = JSONRPCError{
+		Code:    -32602,
+		Message: fmt.Sprintf("Request body too large, max body size %d", maxRequestBodySizeBytes),
+	}
+	errInvalidRequest = func(err error) JSONRPCError {
+		var anyErr any = err
+		return JSONRPCError{
+			Code:    -32602,
+			Message: "Invalid JSON request",
+			Data:    &anyErr,
+		}
+	}
 	errMethodNotFound = JSONRPCError{
 		Code:    -32602,
 		Message: "Method not found",
+	}
+	errSignatureNotFound = JSONRPCError{
+		Code:    -32602,
+		Message: "Signature header not set " + signature.HTTPHeader,
+	}
+	errSignatureNotCorrect = func(err error) JSONRPCError {
+		var anyErr any = err
+		return JSONRPCError{
+			Code:    -32602,
+			Message: "Request signature is not correct",
+			Data:    &anyErr,
+		}
 	}
 )
 
@@ -44,6 +70,31 @@ type JSONRPCError struct {
 	Data    *any   `json:"data,omitempty"`
 }
 
+func respondWithJSONRPCResponse(w http.ResponseWriter, r *http.Request, response *JSONRPCResponse, log *slog.Logger) {
+	w.WriteHeader(http.StatusOK)
+	responseBytes, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, errMarshalResponse, http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	_, err = w.Write(responseBytes)
+	if err != nil {
+		log.Warn("Failed to write response", "err", err)
+	}
+}
+
+func respondWithJSONRPCError(w http.ResponseWriter, r *http.Request, id any, err *JSONRPCError, log *slog.Logger) {
+	resp := JSONRPCResponse{
+		JSONRPC: "2.0",
+		ID:      id,
+		Result:  nil,
+		Error:   err,
+	}
+	respondWithJSONRPCResponse(w, r, &resp, log)
+}
+
 func (prx *Proxy) ServeProxyRequest(w http.ResponseWriter, r *http.Request, publicEndpoint bool) {
 	if r.Method != http.MethodPost {
 		http.Error(w, errMethodNotAllowed, http.StatusMethodNotAllowed)
@@ -59,23 +110,36 @@ func (prx *Proxy) ServeProxyRequest(w http.ResponseWriter, r *http.Request, publ
 
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
-		http.Error(w, errBodySize, http.StatusRequestEntityTooLarge)
+		respondWithJSONRPCError(w, r, nil, &errBodySize, prx.log)
 		return
 	}
-
-	// verify signature here
-	var signer common.Address
 
 	var req JSONRPCRequest
 	err = json.Unmarshal(body, &req)
 	if err != nil {
-		http.Error(w, errInvalidJSON, http.StatusBadRequest)
+		err := errInvalidRequest(err)
+		respondWithJSONRPCError(w, r, nil, &err, prx.log)
 		return
 	}
 
-	jsonrpcErr := prx.HandleRequest(req, signer, publicEndpoint)
+	// verify signature here, if public endpoint and its not signed this will be 0x0
+	var signer common.Address
+	signatureHeader := r.Header.Get(signature.HTTPHeader)
+	if signatureHeader == "" {
+		if !publicEndpoint {
+			respondWithJSONRPCError(w, r, req.ID, &errSignatureNotFound, prx.log)
+			return
+		}
+	} else {
+		signer, err = signature.Verify(signatureHeader, body)
+		if err != nil {
+			err := errSignatureNotCorrect(err)
+			respondWithJSONRPCError(w, r, req.ID, &err, prx.log)
+			return
+		}
+	}
 
-	w.WriteHeader(http.StatusOK)
+	jsonrpcErr := prx.HandleRequest(req, signer, publicEndpoint)
 
 	response := JSONRPCResponse{
 		JSONRPC: "2.0",
@@ -83,22 +147,10 @@ func (prx *Proxy) ServeProxyRequest(w http.ResponseWriter, r *http.Request, publ
 		Result:  nil,
 		Error:   nil,
 	}
-
 	if jsonrpcErr != nil {
 		response.Error = jsonrpcErr
 	}
-
-	responseBytes, err := json.Marshal(response)
-	if err != nil {
-		http.Error(w, errMarshalResponse, http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	_, err = w.Write(responseBytes)
-	if err != nil {
-		prx.log.Warn("Failed to write response", "err", err)
-	}
+	respondWithJSONRPCResponse(w, r, &response, prx.log)
 }
 
 func (prx *Proxy) HandleRequest(req JSONRPCRequest, signer common.Address, publicEndpoint bool) *JSONRPCError {
